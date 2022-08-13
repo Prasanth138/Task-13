307696beaf7ceb4fa5ee68c2e1c9f554bd114460
Prasanth138 <92218925+Prasanth138@users.noreply.github.com>
2022-08-13 20:50:22 +0530

    UPDATE

--- src/components/Sidebars/Sidebar.js
+++ src/components/Sidebars/Sidebar.js
@@ -29,7 +29,7 @@ const Sidebar = ({ sidebarToggle, setSidebarToggle, handleSidebar,handleOpen,han
       {/* <!-- Sidebar - Brand --> */}
       <a
         className="sidebar-brand d-flex align-items-center justify-content-center"
-        href="index.html"
+        href="/"
       >
         <div className="sidebar-brand-icon rotate-n-15">
           <i className="fas fa-laugh-wink"></i>
